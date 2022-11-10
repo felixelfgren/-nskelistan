3c39251b92c5d44ef869262aaaa5448ed3918f76
Simon <Simon.Henriks@awacademy.training>
2022-11-10 10:31:42 +0100

    userRepository

--- src/main/java/com/santashelpers/onskelistan/User.java
+++ src/main/java/com/santashelpers/onskelistan/User.java
@@ -13,4 +13,38 @@ public class User {
     String password;
     String name;
 
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
