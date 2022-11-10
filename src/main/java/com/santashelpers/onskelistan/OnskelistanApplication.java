0e808e1b366acc5e972eb299ecf8bbb6133c23d5
adamskey <adam.lennerling@gmail.com>
2022-11-10 09:44:05 +0100

    Projekt skapat

---
+++ src/main/java/com/santashelpers/onskelistan/OnskelistanApplication.java
@@ -0,0 +1,13 @@
+package com.santashelpers.onskelistan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnskelistanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnskelistanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santashelpers/onskelistan/OnskelistanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santashelpers.onskelistan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnskelistanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
