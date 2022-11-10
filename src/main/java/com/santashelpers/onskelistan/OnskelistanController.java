f256b922f2af1cfa8aa098a9d16902a3125319eb
adamskey <adam.lennerling@gmail.com>
2022-11-10 10:00:04 +0100

    Templates controller

--- src/main/java/com/santashelpers/onskelistan/OnskelistanController.java
+++ src/main/java/com/santashelpers/onskelistan/OnskelistanController.java
@@ -1,4 +1,14 @@
 package com.santashelpers.onskelistan;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OnskelistanController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
 }
